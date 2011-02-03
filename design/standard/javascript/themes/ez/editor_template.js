6b9b0476d8326ee76db227d50a593df723321188
André Rømcke <ar@ez.no>
2011-02-03 15:13:25 +0100

    Fixed #017838: Anchors disappears after publishing

--- design/standard/javascript/themes/ez/editor_template.js
+++ design/standard/javascript/themes/ez/editor_template.js
@@ -1058,7 +1058,7 @@
             {
                 if ( c = cm.get('anchor') )
                 {
-                    c.setActive( !!p && DOM.getAttrib(p, 'id') && !DOM.getAttrib(p, 'href') );
+                    c.setActive( !!p && DOM.getAttrib(p, 'name') && !DOM.getAttrib(p, 'href') );
     
                     if (tinymce.isWebKit)
                     {
@@ -1153,7 +1153,7 @@
                         case 'link':
                             if (v = DOM.getAttrib(n, 'href'))
                                 ti += 'href: ' + v + ' ';
-                            else if (v = DOM.getAttrib(n, 'id'))
+                            else if (v = DOM.getAttrib(n, 'name'))
                                 naa += '#' + v;
                             break;
                         case 'custom':
