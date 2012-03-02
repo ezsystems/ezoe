3add649ef28ace66a41e8ff8cbe827fb6d081a12
Damien Pobel <dp@ez.no>
2012-03-02 17:36:53 +0100

    Fixed #19170: OE creates table cells containing two empty text lines in IE (cherry picked from commit 663731907603093c5ff25749e778e102c629edfa)

--- ezxmltext/handlers/input/ezoexmlinput.php
+++ ezxmltext/handlers/input/ezoexmlinput.php
@@ -1500,7 +1500,8 @@ class eZOEXMLInput extends eZXMLInputHandler
                         if ( $cellContent === '' )
                         {
                             // tinymce has some issues with empty content in some browsers
-                            $cellContent = '<br mce_bogus="1" />';
+                            if ( self::browserSupportsDHTMLType() != 'Trident' )
+                                $cellContent = '<br _mce_bogus="1" />';
                         }
                         if ( $tableCell->nodeName === 'th' )
                         {
